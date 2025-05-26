17cb82e37fac8360b275133a161b464e8f619e94
christian <christianbiringanine22@gmail.com>
2025-05-26 17:32:40 +0200

    Add necessary dependencies but also reajust the config file for a smooth use of internationalization

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import createNextIntlPlugin from "next-intl/plugin";
+import i18nConfig from "./i18n/config";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const withNextIntl = createNextIntlPlugin(i18nConfig);
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
