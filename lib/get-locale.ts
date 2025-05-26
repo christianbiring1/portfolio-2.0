68ca1760284c46cb312dcfde702069932b644906
christian <christianbiringanine22@gmail.com>
2025-05-26 17:30:16 +0200

    Implement the i18n utilities fx

---
+++ lib/get-locale.ts
@@ -0,0 +1,5 @@
+export function getLocale() {
+  // In a real app, you would get this from the request headers or URL
+  // For now, we'll just return "en" as the default
+  return "en";
+}
---
+++ lib/get-messages.ts
@@ -0,0 +1,6 @@
+import { en } from "@/messages/en";
+import { fr } from "@/messages/fr";
+
+export async function getMessages(locale: string) {
+  return locale === "fr" ? fr : en;
+}
