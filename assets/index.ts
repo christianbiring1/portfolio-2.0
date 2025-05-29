4fc7815e9738a94950b90fcad1f8ef07575ed4cc
christian <christianbiringanine22@gmail.com>
2025-05-29 12:43:52 +0200

    Add the different assets

---
+++ assets/index.ts
@@ -0,0 +1,6 @@
+import luna from "./luna.png";
+import fout from "./fout.png";
+import smartElimu from "./smartelimu.png";
+import tryKatch from "./tryKatch.png";
+
+export { luna, fout, smartElimu, tryKatch };
