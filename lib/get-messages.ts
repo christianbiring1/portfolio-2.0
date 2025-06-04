1bc3235c6be218d063a32ee3834bdf2126d766cc
christian <christianbiringanine22@gmail.com>
2025-06-04 12:03:23 +0200

    make the resume available and delete redudant unused files

--- lib/get-messages.ts
+++
@@ -1,6 +0,0 @@
-// import { en } from "@/messages/en";
-// import { fr } from "@/messages/fr";
-
-// export async function getMessages(locale: string) {
-//   return locale === "fr" ? fr : en;
-// }
